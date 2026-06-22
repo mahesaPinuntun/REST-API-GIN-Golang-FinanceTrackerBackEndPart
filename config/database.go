ad5b0e49fe2667b70f3d8de2cd983bedcdee1523
esa <140386522+mahesaPinuntun@users.noreply.github.com>
2026-06-23 05:59:09 +0700

    a

--- config/database.go
+++ config/database.go
@@ -12,16 +12,12 @@ var DB *gorm.DB
 
 func ConnectDB() {
 
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
+	dsn := os.Getenv("DATABASE_URL")
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(
+		postgres.Open(dsn),
+		&gorm.Config{},
+	)
 
 	if err != nil {
 		panic(err)
